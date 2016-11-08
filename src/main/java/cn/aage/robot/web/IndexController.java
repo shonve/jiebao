6b8cc673bd4d7aa84adc5aa92a69ee7743ae63ed
邹湘伟 <zxiangwei@workway.com.cn>
2016-11-08 17:21:03 +0800

    优化

---
+++ src/main/java/cn/aage/robot/web/IndexController.java
@@ -0,0 +1,20 @@
+package cn.aage.robot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by john on 2016/11/8.
+ */
+@Controller
+public class IndexController {
+    @RequestMapping(value = "/index", method = {RequestMethod.POST, RequestMethod.GET})
+    public String index(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
+        return "index";
+
+    }
+}
