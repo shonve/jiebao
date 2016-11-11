28d3ddc55a476a3145bac387d727ab40bb15b816
邹湘伟 <zxiangwei@workway.com.cn>
2016-11-11 17:28:33 +0800

    增加图片控制器

--- src/main/java/cn/aage/robot/service/ImageService.java
+++ src/main/java/cn/aage/robot/service/ImageService.java
@@ -1,8 +1,11 @@
 package cn.aage.robot.service;
 
+import cn.aage.robot.model.file.FileData;
 import cn.aage.robot.repository.file.FileDataRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * Created by john on 2016/11/10.
@@ -11,4 +14,8 @@ import org.springframework.stereotype.Service;
 public class ImageService {
     @Autowired
     private FileDataRepository fileDataRepository;
+
+    public List<FileData> getImages() {
+        return fileDataRepository.findAll();
+    }
 }
--- src/main/java/cn/aage/robot/web/ImageController.java
+++ src/main/java/cn/aage/robot/web/ImageController.java
@@ -1,13 +1,16 @@
 package cn.aage.robot.web;
 
+import cn.aage.robot.model.file.FileData;
 import cn.aage.robot.service.ImageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 /**
  * Created by john on 2016/11/10.
@@ -21,6 +24,13 @@ public class ImageController {
     @RequestMapping(value = "/index", method = {RequestMethod.POST, RequestMethod.GET})
     public String index(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
         return "image/index";
+    }
+
+    @RequestMapping(value = "/images", method = {RequestMethod.POST, RequestMethod.GET})
+    @ResponseBody
+    public List images(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
+        List<FileData> list = imageService.getImages();
+        return list;
     }
 
 }
